3871c0dda9e68c348bbce2e84c3b2e6f1c2d77a3
zewcro <theorousseau75@gmail.com>
2022-03-30 01:24:47 +0200

    add floating btn
    Add floating button to open or close the task adding form

--- src/screens/Tasks/index.js
+++ src/screens/Tasks/index.js
@@ -2,6 +2,7 @@ import { setStatusBarStyle } from 'expo-status-bar';
 import React,  { useState } from 'react'
 import { render } from 'react-dom';
 import { Text, FlatList } from "react-native";
+import FloatingBtn from '../../components/FloatingBtn';
 import Header from "../../components/Header"
 import TaskForm from './TaskForm';
 import TaskTile from './TaskTile';
@@ -9,6 +10,7 @@ import TaskTile from './TaskTile';
 export default function TasksScreen() {
     // Tasks List => Return FlatList => TaskTile
 
+    const [isFormVisible, setIsFormVisible] = useState(false);
     const [tasks, setTasks] = useState([]);
 
     // item = 1 element de l'array ex : {title: "Hello World!", isCompleted: false}
@@ -58,23 +60,25 @@ export default function TasksScreen() {
         setTasks(newTasks);
     }
 
-    // Add a floating button 
-
-
+    const _toggleForm = () => {
+        setIsFormVisible(!isFormVisible)
+    }
+    
     return (
+        <>
         <FlatList 
             ListHeaderComponent={
             <>
             <Header />
-            <TaskForm onAddTask={onAddTask}/>
-            </>
-        }
+           {isFormVisible && <TaskForm onAddTask={onAddTask} />}
+            </>}
             contentContainerStyle={{ flexGrow:1}}
             data={tasks}
             keyExtractor={(item, index) => index.toString() }
             renderItem={renderItem}
         />
-  )
+        <FloatingBtn toggle={_toggleForm} isOpen={isFormVisible} />
+    </>
+  );
 }
-
 
